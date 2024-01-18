9a86511e271d9d15bc93b4c549638df22d1bcf6d
weerapperuma <weerapperumasahiru@gmail.com>
2024-01-18 15:22:56 +0530

    chat app networked

--- client.py
+++ client.py
@@ -1,1 +1,16 @@
-print("hello server")
+import socket
+import threading
+HOST = '127.0.0.1'
+PORT = 1234
+
+def main():
+    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    try:
+        client.connect((HOST,PORT))
+    except:
+        print(f"Unable to connect to server {HOST} {PORT}")
+
+
+if __name__ == '__main__':
+    main()
